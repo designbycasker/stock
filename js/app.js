cf61fdbbd6dc1577b8c7704b6dc96299cca4574e
casker <casker@allwin.bid>
2021-02-05 16:29:19 +0900

    210205_02_

--- js/app.js
+++ js/app.js
@@ -27,7 +27,83 @@ function includeHTML() {
     }
 }
 
-// 캔버스 01
+// ---------------------------------------------------------------------------------------------------
+
+
+// 캔버스 04 - 210205
+const canvas04 = document.querySelector('.canvas_04');
+const context04 = canvas04.getContext('2d');
+
+function clickHandler(e) {
+    console.log(e.layerY);
+    context04.beginPath();
+    context04.arc(e.layerX * 4, e.layerY * 4, 40, 0, Math.PI * 2, true);
+    context04.fill();
+}
+
+canvas04.addEventListener('click', clickHandler);
+
+
+// ---------------------------------------------------------------------------------------------------
+
+
+// 캔버스 03 - 210205
+const canvas03 = document.querySelector('.canvas_03');
+const context03 = canvas03.getContext('2d');
+
+const imgElem = new Image();
+imgElem.src = '../img/issue-1.jpg';
+imgElem.addEventListener('load', () => {
+    context03.drawImage(imgElem, 50, 50);
+})
+
+
+// ---------------------------------------------------------------------------------------------------
+
+
+// 캔버스 02 - 210205
+const canvas02 = document.querySelector('.canvas_02');
+const context02 = canvas02.getContext('2d');
+
+let xPos = 80;
+let count = 0;
+
+function draw() {
+    if (count % 10 === 0) {
+        context02.clearRect(0, 0, 1600, canvas02.height);
+        context02.beginPath();
+        context02.arc(xPos, 580, 40, 0, Math.PI * 2, false);
+        context02.fill();
+        xPos += 32;
+    }
+
+    // 애니메이션 멈춤 01
+    // if (xPos >= canvas02.width - 20) {
+    //     return;
+    // }
+
+    count++;
+    timerId = requestAnimationFrame(draw);
+    // setInterval(draw, 500);
+
+    // 애니메이션 멈춤 02
+    if (xPos >= canvas02.width - 20) {
+        cancelAnimationFrame(timerId);
+    }
+
+}
+draw();
+
+//클릭 시 멈춤
+canvas02.addEventListener('click', () => {
+    cancelAnimationFrame(timerId);
+})
+
+
+// ---------------------------------------------------------------------------------------------------
+
+
+// 캔버스 01 - 210204
 function 라디안(각도) {
     return 각도 * Math.PI / 180;
 }
